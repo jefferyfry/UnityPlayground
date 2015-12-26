771b82ff2b2f71bb33b639969b8f3201bff92550
Jeff Fry <jeff@trackaroo.com>
2015-12-25 22:33:29 -0800

    Added comments to explain code.

--- 3DProject/Assets/FPSInput.cs
+++ 3DProject/Assets/FPSInput.cs
@@ -18,8 +18,8 @@ public class FPSInput : MonoBehaviour {
 		float deltaZ = Input.GetAxis ("Vertical") * speed;
 		Vector3 movement = new Vector3 (deltaX, 0, deltaZ);
 		movement = Vector3.ClampMagnitude (movement, speed);
-		movement *= Time.deltaTime;
-		movement = transform.TransformDirection (movement);
-		characterController.Move (movement);
+		movement *= Time.deltaTime; //compensates for frame speed
+		movement = transform.TransformDirection (movement); //transforms the vector from local coordinate space (character) to global space
+		characterController.Move (movement); //instead of using transform.Translate use CharacterController so that we can get collision detection
 	}
 }
