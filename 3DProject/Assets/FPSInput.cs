7aa31ce2caa11bbfce2dad539610e44440968452
Jeff Fry <jeff@trackaroo.com>
2015-12-25 22:43:21 -0800

    Added gravity.

--- 3DProject/Assets/FPSInput.cs
+++ 3DProject/Assets/FPSInput.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class FPSInput : MonoBehaviour {
 
+	public float gravity = -9.8f;
 	public float speed = 6.0f;
 	private CharacterController characterController;
 
@@ -17,7 +18,8 @@ public class FPSInput : MonoBehaviour {
 		float deltaX = Input.GetAxis ("Horizontal") * speed;
 		float deltaZ = Input.GetAxis ("Vertical") * speed;
 		Vector3 movement = new Vector3 (deltaX, 0, deltaZ);
-		movement = Vector3.ClampMagnitude (movement, speed);
+		movement = Vector3.ClampMagnitude (movement, speed);  //limit movement speed
+		movement.y = gravity;
 		movement *= Time.deltaTime; //compensates for frame speed
 		movement = transform.TransformDirection (movement); //transforms the vector from local coordinate space (character) to global space
 		characterController.Move (movement); //instead of using transform.Translate use CharacterController so that we can get collision detection
