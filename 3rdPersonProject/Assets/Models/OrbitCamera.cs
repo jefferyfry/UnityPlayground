d0798ef2f50227f96ddf4ef113a8e3cc37867145
Jeff Fry <jeffbfry@extrahop.com>
2016-01-01 21:00:24 -0800

    Added orbiting camera.

---
+++ 3rdPersonProject/Assets/Models/OrbitCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitCamera : MonoBehaviour {
+
+	[SerializeField] private Transform target; //this will be the payer gameobject.transform assigned via editor slot
+
+	public float rotSpeed = 1.5f;
+
+	private float rotY;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		rotY = transform.eulerAngles.y; //referring to the camera transform
+		offset = target.position - transform.position; //setting the offset = gameobject.position - camera.position
+	}
+
+	//called after update
+	void LateUpdate () {
+		float horInput = Input.GetAxis ("Horizontal");
+		if (horInput != 0)
+			rotY += horInput * rotSpeed; //keyboard control
+		else
+			rotY += Input.GetAxis ("Mouse X") * rotSpeed * 3; //mouse control
+
+		Quaternion rotation = Quaternion.Euler(0, rotY, 0); //rotation conversion
+		transform.position = target.position - (rotation * offset);
+		transform.LookAt (target); //points camera at the target
+	}
+}
