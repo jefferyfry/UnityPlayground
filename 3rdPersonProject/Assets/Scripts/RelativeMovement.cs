952d6f00a46c41c9556cc10438a9e3a579051240
Jeff Fry <jeffbfry@extrahop.com>
2016-01-02 20:21:21 -0800

    Added jump and fall.

--- 3rdPersonProject/Assets/Scripts/RelativeMovement.cs
+++ 3rdPersonProject/Assets/Scripts/RelativeMovement.cs
@@ -4,10 +4,26 @@ using System.Collections;
 /**
  * Rotates the player object to camera-relative based on arrow key input.
  */
+[RequireComponent(typeof(CharacterController))]
 public class RelativeMovement : MonoBehaviour {
-	[SerializeField] private Transform camera; //the camera object
+	[SerializeField] private Transform orbitCamera; //the camera object
 
 	public float rotSpeed = 15.0f;
+	public float moveSpeed = 6.0f;
+
+	public float jumpSpeed = 15.0f;
+	public float gravity = -9.8f;
+	public float terminalVelocity = -10.0f;
+	public float minFall = -1.5f;
+
+	private float vertSpeed;
+
+	private CharacterController characterController; //character controller provides movement input and collisioning handling
+
+	void Start(){
+		vertSpeed = minFall;
+		characterController = GetComponent<CharacterController>();
+	}
 	
 	// Update is called once per frame
 	void Update() {
@@ -21,11 +37,12 @@ public class RelativeMovement : MonoBehaviour {
 		if (horInput != 0 || vertInput != 0) {
 			movement.x = horInput;
 			movement.z = vertInput;
+			movement = Vector3.ClampMagnitude(movement, moveSpeed);
 
-			Quaternion tmp = camera.rotation;
-			camera.eulerAngles = new Vector3(0, camera.eulerAngles.y, 0);
-			movement = camera.TransformDirection(movement);
-			camera.rotation = tmp;
+			Quaternion tmp = orbitCamera.rotation;
+			orbitCamera.eulerAngles = new Vector3(0, orbitCamera.eulerAngles.y, 0);
+			movement = orbitCamera.TransformDirection(movement);
+			orbitCamera.rotation = tmp;
 
 			// face movement direction
 			//transform.rotation = Quaternion.LookRotation(movement);
@@ -33,5 +50,21 @@ public class RelativeMovement : MonoBehaviour {
 			transform.rotation = Quaternion.Lerp(transform.rotation,
 			                                     direction, rotSpeed * Time.deltaTime);
 		}
+
+		//jumping
+		if(characterController.isGrounded) {
+			if(Input.GetButtonDown("Jump"))
+				vertSpeed = jumpSpeed;
+			else
+				vertSpeed = minFall;
+		} else {
+			vertSpeed += gravity * 5 * Time.deltaTime;
+			if(vertSpeed < terminalVelocity)
+				vertSpeed = terminalVelocity;
+		}
+		movement.y = vertSpeed;
+
+		movement *= Time.deltaTime;
+		characterController.Move(movement);
 	}
 }
